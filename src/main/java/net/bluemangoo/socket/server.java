631b42649e8f0fd4c162e0869552916263dc3ece
3093FengMing <83573615+3093FengMing@users.noreply.github.com>
2022-04-30 11:35:17 +0800

    server.java
    拉果

--- src/main/java/net/bluemangoo/socket/server.java
+++ src/main/java/net/bluemangoo/socket/server.java
@@ -4,46 +4,35 @@
  * @Copyright: 2022 Bluemangoo. All rights reserved.
  * @Description: socket server
  */
+//fork form bluemangoo
 package net.bluemangoo.socket;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class server implements Runnable{
 
-    public void run() {
-        // ServerSocket 实现了 AutoCloseable 接口，所以支持 try-with-resource 语句
-        // 创建一个 ServerSocket，监听 9090 端口
-        try(ServerSocket serv = new ServerSocket(9090)){
-            System.out.printf("Bind Port %d\n", serv.getLocalPort());
-            Socket socket = null;
-            while(true){
-                // 接收连接，如果没有连接，accept() 方法会阻塞
-                socket = serv.accept();
-
-                // 获取输入流，并使用 BufferedInputStream 和 InputStreamReader 装饰，方便以字符流的形式处理，方便一行行读取内容
-                try(BufferedReader in = new BufferedReader( new InputStreamReader(socket.getInputStream()) )){
-                    String msg = null;
-                    char[] cbuf = new char[1024];
-                    int len = 0;
-                    while( (len = in.read(cbuf, 0, 1024)) != -1 ){ // 循环读取输入流中的内容
-                        msg = new String(cbuf, 0, len);
-                        if("Bye".equals(msg)) { // 如果检测到 "Bye" ，则跳出循环，不再读取输入流中内容。
-                            break;
-                        }
-                        System.out.printf("Received Message --> %s \n", msg);
-                    }
-                }catch (IOException e){
-                    e.printStackTrace();
-                }
-
-            }
-
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-    }
+public void run() {
+      try {
+         ServerSocket ss = new ServerSocket(3093);
+         System.out.println("启动服务器于3093端口");
+         Socket s = ss.accept();
+         System.out.println("客户端:"+s.getInetAddress().getLocalHost()+"已连接到服务器");
+         
+         BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+         //读取客户端发送来的消息
+         String mess = br.readLine();
+         System.out.println("客户端："+mess);
+         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+         bw.write(mess+"\n");
+         bw.flush();
+         } catch (IOException e) {
+         e.printStackTrace();
+      }
+   }
 }
